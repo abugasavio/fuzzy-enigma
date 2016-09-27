ec3c336cf5aca0fefbf37d09fb34f09e38c55703
savioabuga <savioabuga@gmail.com>
2016-09-27 10:44:04 +0300

    standard w/ react

---
+++ js/clientApp.jsx
@@ -0,0 +1,15 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+
+const App = () => (
+  <div className='app-container'>
+    <div className='home-info'>
+      <h1 className='title'>svideo</h1>
+      <input className='search' type='text' placeholder='search' />
+      <button className='browse-all'>or Browse all</button>
+    </div>
+  </div>
+  )
+
+ReactDOM.render(<App />, document.getElementById('app'))
+
