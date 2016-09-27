92d34c4d648a832d68be85d867a37d070242c707
savioabuga <savioabuga@gmail.com>
2016-09-27 10:58:16 +0300

    adding react Router

---
+++ js/Landing.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+
+const Landing = () => (
+  <div className='app-container'>
+    <div className='home-info'>
+      <h1 className='title'>svideo</h1>
+      <input className='search' type='text' placeholder='search' />
+      <button className='browse-all'>or Browse all</button>
+    </div>
+  </div>
+  )
+
+module.exports = Landing
+
