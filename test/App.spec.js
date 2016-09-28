0fba5b7aa0985ca8fa32be4c85e1d3989eb9ac24
savioabuga <savioabuga@gmail.com>
2016-09-28 15:24:05 +0300

    testing w/ enzyme

--- test/App.spec.js
+++ test/App.spec.js
@@ -1,10 +1,22 @@
 /* eslint-env mocha */
 
 const { expect } = require('chai')
+const React = require('react')
+const Search = require('../js/Search')
+const { shallow } = require('enzyme')
+const ShowCard = require('../js/ShowCard')
+const { shows } = require('../public/data')
 
 describe('<Search />', () => {
-  xit('should pass', () => {
-    expect(1 + 1 === 2).to.be.true
+  it('should render the brand', () => {
+    const wrapper = shallow(<Search />)
+    // console.log(wrapper.debug())
+    expect(wrapper.contains(<h1 className='brand'>svideo</h1>)).to.be.true
+  })
+
+  it('should render as many shows as there are data are for', () => {
+    const wrapper = shallow(<Search />)
+    expect(wrapper.find(ShowCard).length).to.equal(shows.length)
   })
 })
 
